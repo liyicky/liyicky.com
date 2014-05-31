b123ade57a6e3c46384a90f748986ecf5cc11afc
liyicky <liyicky@gmail.com>
2014-05-31 12:43:48 -0400

    pushing what I have

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Liyicky::Application.routes.draw do
 
-  root 'helloworld#helloworld'
+root 'index#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
