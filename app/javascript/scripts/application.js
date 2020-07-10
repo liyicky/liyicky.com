f7fb51e8496fd547c52ad9d90abb131a1cf86aa6
liyicky <liyicky@gmail.com>
2020-07-10 17:38:15 +0900

    fixes nav

--- app/javascript/scripts/application.js
+++ app/javascript/scripts/application.js
@@ -1,5 +1,5 @@
 $(window).scroll(function() {
-  if ($(document).scrollTop() > 50) {
+  if ($(document).scrollTop() > 55) {
     $('nav').addClass('shrink');
     $('.liy-brand').addClass('liy-brand-shrink')
   } else {
