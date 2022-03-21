fd9a49beb705b6adf6182420ccc9dec4b3a9f908
liyicky <jason@liyicky.com>
2022-03-21 18:39:45 +0900

    added authentication controllers

---
+++ app/controllers/members_controller.rb
@@ -0,0 +1,23 @@
+class MembersController < ApplicationController
+    before_action :authenticate_user!
+
+    def show
+        user = get_user_from_token
+        render json {
+            message: "If you see this, then you did it brother",
+            user: user
+        }
+    end
+
+    private
+
+    def get_user_from_token
+        jwt_payload = JWT.decode(
+            request.headers['Authorization'].split(' ')[1],
+            Rails.application.credentials.devise[:jst_secret_key]
+        ).first
+
+        user_id = jwt_payload["sub"]
+        user = User.find(user_id.to_s)
+    end
+end
---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,22 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+    response_to :json
+
+    private
+
+    def response_with(resource, _opts = {})
+        register_sucess && return if resource.persisted?
+
+        register_failed
+    end
+
+    def register_success
+        render json: {
+            message: "Signed up sucessfully.",
+            user: current_user
+        }, status: :ok
+    end
+
+    def register_failed
+        render json: { message: "Something went wrong." }, status: :unprocessable_entity
+    end
+end
