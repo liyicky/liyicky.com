b08b907edb53ccc4827727da87bad673628429fd
liyicky <jason@liyicky.com>
2022-09-02 12:16:48 +0900

    design implemented

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,6 +1,12 @@
 class IndexController < ApplicationController
 
     def main
-        @blog_posts = BlogPost.all
+        @blog_posts = BlogPost.all.order(:created_at)
+        @latest_post = @blog_posts[0]
+        @second_latest_post = @blog_posts[1]
+        @third_latest_post = @blog_posts[2]
+        @fourth_latest_post = @blog_posts[3]
+
+        @other_posts = @blog_posts.slice(4, 9)
     end
 end
